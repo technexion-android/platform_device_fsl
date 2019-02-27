88217dd7ba52c37f468a49cebc4d1f0ff33c4680
Wig Cheng <wig.cheng@technexion.com>
2019-02-27 16:58:30 +0800

    imx8m: add the idc file of edt-ft5x06 touchscreen to makefile

--- imx8m/ProductConfigCommon.mk
+++ imx8m/ProductConfigCommon.mk
@@ -301,6 +301,7 @@ PRODUCT_COPY_FILES += \
     device/fsl/common/input/eGalax_Touch_Screen.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/HannStar_P1003_Touchscreen.idc \
     device/fsl/common/input/eGalax_Touch_Screen.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Novatek_NT11003_Touch_Screen.idc \
     device/fsl/common/input/eGalax_Touch_Screen.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/eGalax_Touch_Screen.idc \
+    device/fsl/common/input/EP0790M09.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/EP0790M09.idc \
     device/fsl/imx8m/etc/ota.conf:$(TARGET_COPY_OUT_VENDOR)/etc/ota.conf \
     device/fsl/imx8m/init.recovery.freescale.rc:root/init.recovery.freescale.rc \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
