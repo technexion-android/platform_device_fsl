fc6523250a17e5e3d27c275a028c7a5bf31850ef
Wig Cheng <wig.cheng@technexion.com>
2019-04-26 10:21:45 +0800

    pico_imx8m: add 13GB partition table to makefile

--- imx8m/pico_imx8m/BoardConfig.mk
+++ imx8m/pico_imx8m/BoardConfig.mk
@@ -45,7 +45,9 @@ TARGET_RECOVERY_FSTAB = $(IMX_DEVICE_PATH)/fstab.freescale
 # Support gpt
 BOARD_BPT_INPUT_FILES += device/fsl/common/partition/device-partitions-13GB-ab.bpt
 ADDITION_BPT_PARTITION = partition-table-7GB:device/fsl/common/partition/device-partitions-7GB-ab.bpt \
-                         partition-table-28GB:device/fsl/common/partition/device-partitions-28GB-ab.bpt
+                         partition-table-28GB:device/fsl/common/partition/device-partitions-28GB-ab.bpt \
+                         partition-table-13GB:device/fsl/common/partition/device-partitions-13GB-ab.bpt
+
 
 
 # Vendor Interface manifest and compatibility
