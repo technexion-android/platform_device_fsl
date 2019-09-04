4dfe67fbe7c15245857938231b89f94eeb1c7c57
Wig Cheng <wig.cheng@technexion.com>
2019-09-04 16:51:51 +0800

    pico_imx7: add virtual-image generator support to makefile

--- imx7d/pico_imx7/pico_imx7.mk
+++ imx7d/pico_imx7/pico_imx7.mk
@@ -99,7 +99,8 @@ PRODUCT_COPY_FILES += \
     device/fsl/common/tools/fastboot_imx_flashall.sh:fastboot_imx_flashall.sh \
     device/fsl/common/tools/fsl-sdcard-partition.sh:fsl-sdcard-partition.sh \
     device/fsl/common/tools/uuu_imx_android_flash.bat:uuu_imx_android_flash.bat \
-    device/fsl/common/tools/uuu_imx_android_flash.sh:uuu_imx_android_flash.sh
+    device/fsl/common/tools/uuu_imx_android_flash.sh:uuu_imx_android_flash.sh \
+    device/fsl/common/tools/fsl-sdcard-partition-virtual-image.sh:fsl-sdcard-partition-virtual-image.sh
 
 # tn-init file
 PRODUCT_COPY_FILES += \
