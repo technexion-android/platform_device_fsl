76db7ea85d0245498b6a8de704265fb1638cc660
Wig Cheng <wig.cheng@technexion.com>
2020-10-27 10:38:35 +0800

    edm-g-imx8mp: initialize device support
    Copy from evk_8mp, just initial level, not tweak yet.

---
+++ imx8m/edm_g_imx8mp/SharedBoardConfig.mk
@@ -0,0 +1,26 @@
+KERNEL_NAME := Image
+TARGET_KERNEL_ARCH := arm64
+
+#Enable this to config 1GB ddr on evk_imx8mp
+#LOW_MEMORY := true
+
+#Enable this to include trusty support
+PRODUCT_IMX_TRUSTY := true
+
+#Enable this to disable product partition build.
+#IMX_NO_PRODUCT_PARTITION := true
+
+#NXP 8997 wifi driver module
+BOARD_VENDOR_KERNEL_MODULES += \
+    $(KERNEL_OUT)/drivers/net/wireless/marvell/mrvl8997/wlan_src/mlan.ko \
+    $(KERNEL_OUT)/drivers/net/wireless/marvell/mrvl8997/wlan_src/pcie8xxx.ko \
+# mipi-panel touch driver module
+BOARD_VENDOR_KERNEL_MODULES += \
+    $(KERNEL_OUT)/drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_i2c.ko
+
+# isp vvcam driver module
+BOARD_VENDOR_KERNEL_MODULES += \
+    $(TARGET_OUT_INTERMEDIATES)/VVCAM_OBJ/vvcam-video.ko \
+    $(TARGET_OUT_INTERMEDIATES)/VVCAM_OBJ/vvcam-isp.ko \
+    $(TARGET_OUT_INTERMEDIATES)/VVCAM_OBJ/vvcam-dwe.ko \
+    $(TARGET_OUT_INTERMEDIATES)/VVCAM_OBJ/basler-camera-driver-vvcam.ko
