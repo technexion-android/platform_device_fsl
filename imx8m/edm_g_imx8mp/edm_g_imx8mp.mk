50b92f1e6836849535d765e81844ad448a8618a6
Wig Cheng <wig.cheng@technexion.com>
2020-11-05 15:47:01 +0800

    edm-g-imx8mp: add demo image generator to output folder

--- imx8m/edm_g_imx8mp/edm_g_imx8mp.mk
+++ imx8m/edm_g_imx8mp/edm_g_imx8mp.mk
@@ -151,6 +151,7 @@ PRODUCT_COPY_FILES += \
     device/fsl/common/tools/fastboot_imx_flashall.bat:fastboot_imx_flashall.bat \
     device/fsl/common/tools/fastboot_imx_flashall.sh:fastboot_imx_flashall.sh \
     device/fsl/common/tools/fsl-sdcard-partition.sh:fsl-sdcard-partition.sh \
+    device/fsl/common/tools/fsl-sdcard-partition-gen_image.sh:fsl-sdcard-partition-gen_image.sh \
     device/fsl/common/tools/uuu_imx_android_flash.bat:uuu_imx_android_flash.bat \
     device/fsl/common/tools/uuu_imx_android_flash.sh:uuu_imx_android_flash.sh
 
