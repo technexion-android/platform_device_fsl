9c7c27e4f1fafd89b6cee911420eaf6ed86bb291
Wig Cheng <wig.cheng@technexion.com>
2021-05-05 14:20:22 +0800

    pico_imx8mm: remove while loop from tn_init service
    init.rc has a detection method to get property status, so no need
    additional while loop to get status, it will increase cpu usage.

--- imx8m/pico_imx8mm/boot/tn_init.sh
+++ imx8m/pico_imx8mm/boot/tn_init.sh
@@ -2,24 +2,18 @@
 
 ifconfig eth0 up
 
-# detect rotation
-while :
-do
-  if [[ "$(getprop init.svc.bootanim)" == "stopped"  ]];then
-    sleep 6
+# screen rotation
+sleep 5
 
-    if [[ "$(getprop ro.boot.hwrotation)" == "0" ]];then
-      rot_value=1
-    elif [[ "$(getprop ro.boot.hwrotation)" == "90" ]];then
-      rot_value=2
-    elif [[ "$(getprop ro.boot.hwrotation)" == "180" ]];then
-      rot_value=3
-    elif [[ "$(getprop ro.boot.hwrotation)" == "270" ]];then
-      rot_value=0
-    fi
-    # 1 = 0 degree, 2 = 90 degree, 3 = 180 degree, 0 = 270 degree
-    content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:"$rot_value"
-    break;
-  fi
-  sleep 2
-done
+if [[ "$(getprop ro.boot.swrotation)" == "0" ]];then
+  rot_value=0
+elif [[ "$(getprop ro.boot.swrotation)" == "90" ]];then
+  rot_value=1
+elif [[ "$(getprop ro.boot.swrotation)" == "180" ]];then
+  rot_value=2
+elif [[ "$(getprop ro.boot.swrotation)" == "270" ]];then
+  rot_value=3
+fi
+
+# 0 = 0 degree, 1 = 90 degree, 2 = 180 degree, 3 = 270 degree
+content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:"$rot_value"
