bfb3f421163ac175d5ff7bbbe1d36a2c8c8b0002
Wig Cheng <wig.cheng@technexion.com>
2021-06-23 10:05:23 +0800

    edm_g_imx8mp: fix ril driver cannot detect right node bug

--- imx8m/edm_g_imx8mp/UbootKernelBoardConfig.mk
+++ imx8m/edm_g_imx8mp/UbootKernelBoardConfig.mk
@@ -12,8 +12,11 @@ TARGET_BOOTLOADER_CONFIG := imx8mp:edm-g-imx8mp_android_defconfig
 TARGET_KERNEL_DEFCONFIG := tn_android_defconfig
 #endif
 
+ifeq ($(SIM8202_MODEM_ACTIVE),true)
+TARGET_KERNEL_ADDITION_DEFCONF := android_sim8202_addition_defconfig
+else
 TARGET_KERNEL_ADDITION_DEFCONF := android_addition_defconfig
-
+endif
 
 # absolute path is used, not the same as relative path used in AOSP make
 TARGET_DEVICE_DIR := $(patsubst %/, %, $(dir $(realpath $(lastword $(MAKEFILE_LIST)))))
