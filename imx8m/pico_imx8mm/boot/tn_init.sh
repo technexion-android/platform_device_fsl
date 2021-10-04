af9c24f1ad87c9e39ac8b5c2decb83a04484289f
Wig Cheng <wig.cheng@technexion.com>
2021-10-04 15:51:04 +0800

    pico-imx8mm: change 5" mipi lcd detect way using 'wm' command on tn_init

--- imx8m/pico_imx8mm/boot/tn_init.sh
+++ imx8m/pico_imx8mm/boot/tn_init.sh
@@ -5,15 +5,8 @@ ifconfig eth0 up
 # screen rotation
 sleep 5
 
-if [[ "$(getprop ro.boot.swrotation)" == "0" ]];then
-  rot_value=0
-elif [[ "$(getprop ro.boot.swrotation)" == "90" ]];then
+if [[ "$(wm size | grep 720x1280)" ]];then
   rot_value=1
-elif [[ "$(getprop ro.boot.swrotation)" == "180" ]];then
-  rot_value=2
-elif [[ "$(getprop ro.boot.swrotation)" == "270" ]];then
-  rot_value=3
+  # 0 = 0 degree, 1 = 90 degree, 2 = 180 degree, 3 = 270 degree
+  content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:"$rot_value"
 fi
-
-# 0 = 0 degree, 1 = 90 degree, 2 = 180 degree, 3 = 270 degree
-content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:"$rot_value"
