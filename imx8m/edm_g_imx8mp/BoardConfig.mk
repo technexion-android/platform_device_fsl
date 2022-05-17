870de7d27f40f9bc9d8a930ae071d17f3774e11b
Wig Cheng <wig.cheng@technexion.com>
2022-05-17 03:06:23 +0000

    edm_g_imx8mp: initialized device folder for boot up stage
    Copy from evk_8mp and did modification, let it can boot up success as
    first target.

---
+++ imx8m/edm_g_imx8mp/BoardConfig.mk
@@ -0,0 +1,153 @@
+# -------@block_infrastructure-------
+#
+# Product-specific compile-time definitions.
+#
+
+include $(CONFIG_REPO_PATH)/imx8m/BoardConfigCommon.mk
+
+# -------@block_common_config-------
+#
+# SoC-specific compile-time definitions.
+#
+
+BOARD_SOC_TYPE := IMX8MP
+BOARD_HAVE_VPU := true
+BOARD_VPU_TYPE := hantro
+HAVE_FSL_IMX_GPU2D := false
+HAVE_FSL_IMX_GPU3D := true
+HAVE_FSL_IMX_PXP := false
+TARGET_USES_HWC2 := true
+TARGET_HAVE_VULKAN := true
+CFG_SECURE_IOCTRL_REGS := true
+ENABLE_SEC_DMABUF_HEAP := true
+
+SOONG_CONFIG_IMXPLUGIN += \
+                        BOARD_VPU_TYPE \
+                        CFG_SECURE_IOCTRL_REGS \
+                        ENABLE_SEC_DMABUF_HEAP
+
+SOONG_CONFIG_IMXPLUGIN_BOARD_SOC_TYPE = IMX8MP
+SOONG_CONFIG_IMXPLUGIN_BOARD_HAVE_VPU = true
+SOONG_CONFIG_IMXPLUGIN_BOARD_VPU_TYPE = hantro
+SOONG_CONFIG_IMXPLUGIN_BOARD_VPU_ONLY = false
+SOONG_CONFIG_IMXPLUGIN_PREBUILT_FSL_IMX_CODEC = true
+SOONG_CONFIG_IMXPLUGIN_POWERSAVE = $(POWERSAVE)
+SOONG_CONFIG_IMXPLUGIN_CFG_SECURE_IOCTRL_REGS = true
+SOONG_CONFIG_IMXPLUGIN_ENABLE_SEC_DMABUF_HEAP = true
+
+# -------@block_memory-------
+USE_ION_ALLOCATOR := true
+USE_GPU_ALLOCATOR := false
+
+# -------@block_storage-------
+
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# use sparse image.
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
+
+# Support gpt
+
+BOARD_BPT_INPUT_FILES += device/nxp/common/partition/device-partitions-13GB-ab_super.bpt
+  ADDITION_BPT_PARTITION = partition-table-28GB:device/nxp/common/partition/device-partitions-28GB-ab_super.bpt \
+                          partition-table-13GB:device/nxp/common/partition/device-partitions-13GB-ab_super.bpt
+
+
+BOARD_PREBUILT_DTBOIMAGE := $(OUT_DIR)/target/product/$(PRODUCT_DEVICE)/dtbo-imx8mp.img
+
+BOARD_USES_METADATA_PARTITION := true
+BOARD_ROOT_EXTRA_FOLDERS += metadata
+
+# -------@block_security-------
+ENABLE_CFI=false
+
+BOARD_AVB_ENABLE := true
+BOARD_AVB_ALGORITHM := SHA256_RSA4096
+# The testkey_rsa4096.pem is copied from external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_KEY_PATH := $(CONFIG_REPO_PATH)/common/security/testkey_rsa4096.pem
+
+BOARD_AVB_BOOT_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_BOOT_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_BOOT_ROLLBACK_INDEX_LOCATION := 2
+
+# -------@block_treble-------
+# Vendor Interface manifest and compatibility
+ifeq ($(POWERSAVE),true)
+    DEVICE_MANIFEST_FILE := $(IMX_DEVICE_PATH)/manifest_powersave.xml
+else
+    DEVICE_MANIFEST_FILE := $(IMX_DEVICE_PATH)/manifest.xml
+endif
+
+DEVICE_MATRIX_FILE := $(IMX_DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(IMX_DEVICE_PATH)/device_framework_matrix.xml
+
+# -------@block_wifi-------
+# NXP 8997 WIFI
+BOARD_WLAN_DEVICE            := nxp
+WPA_SUPPLICANT_VERSION       := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER  := NL80211
+BOARD_HOSTAPD_DRIVER         := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB               := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
+# -------@block_bluetooth-------
+# NXP 8997 BT
+BOARD_HAVE_BLUETOOTH_NXP := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(IMX_DEVICE_PATH)/bluetooth
+
+# -------@block_sensor-------
+BOARD_USE_SENSOR_FUSION := true
+
+# -------@block_kernel_bootimg-------
+BOARD_KERNEL_BASE := 0x40400000
+
+CMASIZE=800M
+# NXP default config
+BOARD_KERNEL_CMDLINE := init=/init firmware_class.path=/vendor/firmware loop.max_part=7 bootconfig
+BOARD_BOOTCONFIG += androidboot.console=ttymxc1 androidboot.hardware=nxp
+
+# memory config
+BOARD_KERNEL_CMDLINE += transparent_hugepage=never
+BOARD_KERNEL_CMDLINE += swiotlb=65536
+
+# display config
+BOARD_BOOTCONFIG += androidboot.lcd_density=240 androidboot.primary_display=imx-drm
+
+# wifi config
+BOARD_BOOTCONFIG += androidboot.wificountrycode=CN
+ifeq ($(POWERSAVE),true)
+    BOARD_KERNEL_CMDLINE +=  moal.mod_para=wifi_mod_para_powersave.conf pci=nomsi
+else
+    BOARD_KERNEL_CMDLINE +=  moal.mod_para=wifi_mod_para.conf pci=nomsi
+endif
+
+# low memory device build config
+ifeq ($(LOW_MEMORY),true)
+BOARD_KERNEL_CMDLINE += cma=320M@0x400M-0xb80M galcore.contiguousSize=33554432
+BOARD_BOOTCONFIG += androidboot.displaymode=720p
+else
+BOARD_KERNEL_CMDLINE += cma=$(CMASIZE)@0x400M-0x1000M
+endif
+
+# powersave config
+ifeq ($(POWERSAVE),true)
+    BOARD_BOOTCONFIG += androidboot.powersave.usb=true androidboot.powersave.uclamp=true androidboot.powersave.lpa=true
+endif
+
+ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
+BOARD_BOOTCONFIG += androidboot.vendor.sysrq=1
+endif
+
+TARGET_BOARD_DTS_CONFIG := imx8mp:imx8mp-edm-g-wb.dtb
+
+ALL_DEFAULT_INSTALLED_MODULES += $(BOARD_VENDOR_KERNEL_MODULES)
+
+# -------@block_sepolicy-------
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    $(CONFIG_REPO_PATH)/imx8m/system_ext_pri_sepolicy
+
+BOARD_SEPOLICY_DIRS := \
+       $(CONFIG_REPO_PATH)/imx8m/sepolicy \
+       $(IMX_DEVICE_PATH)/sepolicy
+
