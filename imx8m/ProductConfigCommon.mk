0566005a33b73ced206fee883338f5c2c3a11e78
Wig Cheng <wig.cheng@technexion.com>
2022-05-23 07:11:56 +0000

    imx8m: add libgpiod and relate utilities to global makefile

--- imx8m/ProductConfigCommon.mk
+++ imx8m/ProductConfigCommon.mk
@@ -298,6 +298,17 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+
+# tn custom utils/apps
+PRODUCT_PACKAGES += \
+    libgpiod \
+    gpiodetect \
+    gpiofind \
+    gpioget \
+    gpioinfo \
+    gpiomon \
+    gpioset
+
 # -------@block_wifi-------
 PRODUCT_PACKAGES += \
     hostapd \
