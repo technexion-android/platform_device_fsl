ce87da2fc8073a89eb0ef295524b51b01ccc631e
Sean Xie <sean.xie@technexion.com>
2023-07-14 19:28:24 +0800

    edm-g-imx8mm: upgrade to android13

--- imx8m/edm_g_imx8mm/UbootKernelBoardConfig.mk
+++ imx8m/edm_g_imx8mm/UbootKernelBoardConfig.mk
@@ -1,10 +1,18 @@
+# from BoardConfig.mk
 TARGET_BOOTLOADER_POSTFIX := bin
 UBOOT_POST_PROCESS := true
 
 # u-boot target
 TARGET_BOOTLOADER_CONFIG := imx8mm:edm-g-imx8mm_android_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8mm-evk-uuu:edm-g-imx8mm_android_defconfig
 
+# imx8mm kernel defconfig
+ifeq ($(IMX8MM_USES_GKI),true)
+TARGET_KERNEL_DEFCONFIG := gki_defconfig
+TARGET_KERNEL_GKI_DEFCONF:= tn_imx8mm_gki.fragment
+else
 TARGET_KERNEL_DEFCONFIG := tn_imx8_android_defconfig
+endif
 
 TARGET_KERNEL_ADDITION_DEFCONF := android_addition_defconfig
 
