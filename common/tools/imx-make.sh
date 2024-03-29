3d4ce9fe30ec86e2bd3386a68be72337cd895218
Tifa Chen <tifa.chen@technexion.com>
2024-03-28 19:53:35 -0700

    common: Add QCA9377 qcacld-2.0 out of tree driver module build
    Usage:
    1. build within kernel
       ./imx-make.sh kernel
    
    2. qcacld-2.0 module build
       ./imx-make.sh qcacld
    
    3. qcacld-2.0 clean module build
       ./imx-make.sh qcacld -c

--- common/tools/imx-make.sh
+++ common/tools/imx-make.sh
@@ -16,6 +16,7 @@ cat << EOF
            galcore                 galcore.ko in GPU repo will be compiled
            vvcam                   vvcam.ko, the ISP driver will be compiled
            mxmwifi                 mlan.ko moal.ko, the MXMWifi driver will be compiled
+           qcacld                  wlan.ko, the qcacld-2.0 driver will be compiled
            dtboimage               dtbo images will be built out
            bootimage               boot.img will be built out
            vendorbootimage         vendor_boot.img will be built out
@@ -73,6 +74,7 @@ build_kernel_oot_module_flag=0
 build_galcore=""
 build_vvcam=""
 build_mxmwifi=""
+build_qcacld=""
 build_bootimage=""
 build_vendorbootimage=""
 build_dtboimage=""
@@ -98,6 +100,8 @@ for arg in ${args[*]} ; do
                     build_vvcam="vvcam";;
         mxmwifi) build_kernel_oot_module_flag=1
                     build_mxmwifi="mxmwifi";;
+        qcacld) build_kernel_oot_module_flag=1
+                    build_qcacld="qcacld";;
         bootimage) build_android_flag=1;
                     build_kernel="${OUT}/kernel";
                     build_bootimage="bootimage";;
@@ -140,6 +144,12 @@ if [ -n "${build_kernel_modules}" ]; then
     build_kernel_oot_module_flag=1;
 fi
 
+# wlan.ko need build with kernel each time to make sure "insmod wlan.ko" works
+if [ -n "${build_kernel_modules}" ]; then
+    build_qcacld="qcacld";
+    build_kernel_oot_module_flag=1;
+fi
+
 product_makefile=`pwd`/`find device/nxp -maxdepth 4 -name "${TARGET_PRODUCT}.mk"`;
 product_path=${product_makefile%/*}
 soc_path=${product_path%/*}
@@ -177,7 +187,7 @@ fi
 if [ ${build_kernel_oot_module_flag} -eq 1 ] || [ -n "${build_kernel_modules}" ]; then
     soc_path=${soc_path} product_path=${product_path} nxp_git_path=${nxp_git_path} clean_build=${clean_build} \
         skip_config_or_clean=${skip_config_or_clean} make -C ./ -f ${nxp_git_path}/common/build/Makefile ${parallel_option} \
-        ${build_vvcam} ${build_galcore} ${build_mxmwifi} </dev/null || exit
+        ${build_vvcam} ${build_galcore} ${build_mxmwifi} ${build_qcacld} </dev/null || exit
 fi
 
 if [ ${build_android_flag} -eq 1 ] || [ ${build_whole_android_flag} -eq 1 ]; then
