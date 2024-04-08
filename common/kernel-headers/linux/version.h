e1e2fc7736e1b5f53a4c30425b92a5ad0ea41e42
Jindong Yue <jindong.yue@nxp.com>
2024-04-08 17:48:45 +0900

    MA-21719-3 Update kernel headers for 6.6 kernel
    Command:
        ./device/nxp/common/tools/update_kerenl_header.sh
    
    Note:
    After running the script, remember to check for any changes
    in vendor/nxp-opensource/libdrm-imx, and if so, submit a patch.
    
    Change-Id: I3bbecf3f0fdf66da5af7a700465294f75152379b
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/24363
    Reviewed-by: Zhipeng Wang <zhipeng.wang_1@nxp.com>

--- common/kernel-headers/linux/version.h
+++ common/kernel-headers/linux/version.h
@@ -16,8 +16,8 @@
  ***
  ****************************************************************************
  ****************************************************************************/
-#define LINUX_VERSION_CODE 394762
+#define LINUX_VERSION_CODE 394775
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + ((c) > 255 ? 255 : (c)))
 #define LINUX_VERSION_MAJOR 6
 #define LINUX_VERSION_PATCHLEVEL 6
-#define LINUX_VERSION_SUBLEVEL 10
+#define LINUX_VERSION_SUBLEVEL 23
