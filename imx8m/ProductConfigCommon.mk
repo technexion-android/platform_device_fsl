8b118e2032318e9f226bec58d7fe4c3393ec7c89
Tifa Chen <tifa.chen@technexion.com>
2024-04-16 22:34:29 -0700

    imx8m: add package libbt-vendor-nxp
    QCA9377 & IW417 wifi driver one image implementation (2)
    
    1. package both wifi driver and firmware
    2. package both bt hal library and firmware config
    
    Also need hardware/nxp/bt commit

--- imx8m/ProductConfigCommon.mk
+++ imx8m/ProductConfigCommon.mk
@@ -339,7 +339,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # -------@block_bluetooth-------
 
 PRODUCT_PACKAGES += \
-     libbt-vendor
+     libbt-vendor \
+     libbt-vendor-nxp
 
 # LDAC codec
 PRODUCT_PACKAGES += \
