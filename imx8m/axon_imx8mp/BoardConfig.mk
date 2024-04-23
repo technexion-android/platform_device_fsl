25ef19fe9b9ce40a2ed1449b17be12bbea8643ab
Tifa Chen <tifa.chen@technexion.com>
2024-04-22 17:58:41 -0700

    axon_imx8mp: package audio module driver tlv320 file

--- imx8m/axon_imx8mp/BoardConfig.mk
+++ imx8m/axon_imx8mp/BoardConfig.mk
@@ -175,6 +175,8 @@ BOARD_HAVE_BLUETOOTH_NXP := true
 
 endif
 
+# -------@block_Audio-------
+BOARD_VENDOR_KERNEL_MODULES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/sound/soc/codecs/snd-soc-tlv320aic31xx.ko
 
 # -------@block_sensor-------
 BOARD_USE_SENSOR_FUSION := false
