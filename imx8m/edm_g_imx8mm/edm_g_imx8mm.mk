30f4bfe5662f7d788dc5dac1d132e556c72898c5
Tifa Chen <tifa.chen@technexion.com>
2024-04-30 00:57:24 -0700

    edm_g_imx8mm: auto bt attach during boot up
    QCA9377 & IW417 wifi driver one image implementation (4)
    
    1. package both wifi driver and firmware
    2. package both bt hal library and firmware config
    3. dynamic loading wifi driver during boot up
    4. dynamic loading bt library at bt interface framework
    
    After BT_Wifi driver installed, Check the vendor and write into system property "vendor.all.wifi_bt_device".
    When bluetooth enable, service will dynamic load bt library by this system property.

--- imx8m/edm_g_imx8mm/edm_g_imx8mm.mk
+++ imx8m/edm_g_imx8mm/edm_g_imx8mm.mk
@@ -116,7 +116,8 @@ PRODUCT_COPY_FILES += \
     $(IMX_DEVICE_PATH)/early.init.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/early.init.cfg \
     $(LINUX_FIRMWARE_IMX_PATH)/linux-firmware-imx/firmware/sdma/sdma-imx7d.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/firmware/imx/sdma/sdma-imx7d.bin \
     $(CONFIG_REPO_PATH)/common/init/init.insmod.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.insmod.sh \
-    $(IMX_DEVICE_PATH)/ueventd.nxp.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
+    $(IMX_DEVICE_PATH)/ueventd.nxp.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc \
+    $(CONFIG_REPO_PATH)/common/init/btattach.sh:$(TARGET_COPY_OUT_VENDOR)/bin/btattach.sh
 
 
 # -------@block_storage-------
