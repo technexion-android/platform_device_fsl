3a1853ad3fa8ee06855925e843ac948798530997
yunjie <yunjie.jia@nxp.com>
2024-05-06 15:38:38 +0000

    MA-22407 [Android i.MX 95] enable WiFi/BT on i.MX 95 Toradex Verdin board
    Reason:the default wifi BT module on i.MX 95 Verdin board is AW271 which
    is part of IW612, so need to copy IW612 FW to /vendor/firmware
    Tested: on i.MX 95 Verdin board
    
    Change-Id: I717b946459c3562644602cd1cbdf5b093553f696
    Signed-off-by: yunjie <yunjie.jia@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/24796
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- imx9/evk_95/evk_95.mk
+++ imx9/evk_95/evk_95.mk
@@ -371,6 +371,7 @@ PRODUCT_PACKAGES += \
 # nxp 8997 wifi and bluetooth combo Firmware
 PRODUCT_COPY_FILES += \
     vendor/nxp/imx-firmware/nxp/FwImage_9098_PCIE/pcieuart9098_combo_v1.bin:vendor/firmware/pcieuart9098_combo_v1.bin \
+    vendor/nxp/imx-firmware/nxp/FwImage_IW612_SD/sduart_nw61x_v1.bin.se:vendor/firmware/sduart_nw61x_v1.bin.se \
     vendor/nxp/imx-firmware/nxp/android_wifi_mod_para.conf:vendor/firmware/wifi_mod_para.conf
 
 # Wifi regulatory
