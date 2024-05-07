9b544c26c6c057803e5ca72504b68588cb64311a
He Zhai <zhai.he@nxp.com>
2024-05-07 17:14:30 +0900

    MA-22361-6 Copy keys related to firmwareloader to OUT directory
    Copy keys related to firmwareloader to OUT directory, so that
    test team can obtain them from daily image.
    
    Change-Id: I8e22978093d12a967f6dc1320c0667b2c016acb3
    Signed-off-by: He Zhai <zhai.he@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/24823
    Reviewed-by: Ji Luo <ji.luo@nxp.com>

--- imx9/evk_95/evk_95.mk
+++ imx9/evk_95/evk_95.mk
@@ -537,3 +537,9 @@ else
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
 endif
 PRODUCT_SOONG_NAMESPACES += vendor/partner_gms
+
+ifeq ($(PRODUCT_IMX_TRUSTY),true)
+PRODUCT_COPY_FILES += \
+    $(CONFIG_REPO_PATH)/common/security/firmware_encrypt_key.bin:firmware_test_keys/firmware_encrypt_key.bin  \
+    $(CONFIG_REPO_PATH)/common/security/firmware_public_key.der:firmware_test_keys/firmware_public_key.der
+endif
