f7b90cf3ba9aac43a1c2ae0f78cd320f49527a31
Tifa Chen <tifa.chen@technexion.com>
2024-06-16 19:39:39 -0700

    tek_imx8mp: Add tek_imx8mp support on Android
    Copy from axon_imx8mp and modify for first boot up stage.
    - Add qca9377-5(pcie) wifi driver and update it's frimware
    - Add qca9377-5(pcie) bluetooth driver and update it's firmware
    - Update dtbo files

--- imx8m/AndroidProducts.mk
+++ imx8m/AndroidProducts.mk
@@ -33,7 +33,8 @@ PRODUCT_MAKEFILES := \
   $(LOCAL_DIR)/evk_8mp/evk_8mp.mk \
   $(LOCAL_DIR)/edm_g_imx8mp/edm_g_imx8mp.mk \
   $(LOCAL_DIR)/edm_g_imx8mm/edm_g_imx8mm.mk \
-  $(LOCAL_DIR)/axon_imx8mp/axon_imx8mp.mk
+  $(LOCAL_DIR)/axon_imx8mp/axon_imx8mp.mk \
+  $(LOCAL_DIR)/tek_imx8mp/tek_imx8mp.mk
 
 COMMON_LUNCH_CHOICES := \
   evk_8mq-user \
@@ -49,4 +50,6 @@ COMMON_LUNCH_CHOICES := \
   edm_g_imx8mm-user \
   edm_g_imx8mm-userdebug \
   axon_imx8mp-user \
-  axon_imx8mp-userdebug
+  axon_imx8mp-userdebug \
+  tek_imx8mp-user \
+  tek_imx8mp-userdebug
