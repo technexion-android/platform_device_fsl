815f3fb941cea871c065b72e61ff31c128005136
Tifa Chen <tifa.chen@technexion.com>
2024-07-03 02:18:59 -0700

    imx8m: packge exc3000 touch module driver
    In tn_imx8_android_defconfig, exc3000 build as module driver.
    We have to package manually.

--- imx8m/edm_g_imx8mm/BoardConfig.mk
+++ imx8m/edm_g_imx8mm/BoardConfig.mk
@@ -151,6 +151,8 @@ BOARD_HAVE_BLUETOOTH_NXP := true
 # -------@block_sensor-------
 BOARD_USE_SENSOR_FUSION := false
 
+# -------@block_touch-------
+BOARD_VENDOR_KERNEL_MODULES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/drivers/input/touchscreen/exc3000.ko
 
 # -------@block_kernel_bootimg-------
 BOARD_KERNEL_BASE := 0x40400000
