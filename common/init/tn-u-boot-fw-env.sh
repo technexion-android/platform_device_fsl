69117b3d0adb74dc9578eaa17e7efc5bd17bae13
Tifa Chen <tifa.chen@technexion.com>
2024-07-16 19:19:06 -0700

    common: Add tn-u-boot-fw-env.sh for fw-env service
    This script will do followings for fw_printenv envtool feature.
    1. move necessary librarys
    2. chmod for specified library
    3. generate config file

---
+++ common/init/tn-u-boot-fw-env.sh
@@ -0,0 +1,26 @@
+#! /vendor/bin/sh
+
+cp /vendor/lib64/ld-linux-aarch64.so.1 /data/vendor/fw_env
+cp /vendor/lib64/libc.so.6 /data/vendor/fw_env
+chmod a+x /data/vendor/fw_env/ld-linux-aarch64.so.1
+
+MMC_TAR=$(cat /proc/cmdline | grep -io "mmcblk[[:digit:]]")
+QSPI_BOOT=$(cat /proc/cmdline  |grep -oi "qspi_boot=yes")
+ARCH=$(uname -m)
+
+if [ -z "$MMC_TAR" ]; then
+	echo "Can not find MMC device path from kernel argument!"
+	#exit 1
+fi
+
+if [[ -n ${QSPI_BOOT} ]];then
+	BOOT_DEV='/dev/mtd0'
+else
+	BOOT_DEV='/dev/block/'${MMC_TAR} 
+fi
+
+if [ ${ARCH} == 'aarch64' ];then
+	echo -e "${BOOT_DEV}\t0x400000\t0x4000" > /data/vendor/fw_env/fw_env.config
+else
+	echo -e "${BOOT_DEV}\t0xc0000\t0x2000" > /data/vendor/fw_env/fw_env.config
+fi
