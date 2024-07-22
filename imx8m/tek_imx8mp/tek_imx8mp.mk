8c12e07e710e20fecbdade5f98ebd478b9aace00
Tifa Chen <tifa.chen@technexion.com>
2024-07-22 01:28:25 -0700

    tek_imx8mp: Add fw_printenv env tool support
    Package envtools and it's related files.
    Use macro define "FW_ENV_SUPPORT" to enable envtool feature.

--- imx8m/tek_imx8mp/tek_imx8mp.mk
+++ imx8m/tek_imx8mp/tek_imx8mp.mk
@@ -779,3 +779,14 @@ PRODUCT_PACKAGES += hollow
 
 # sets vendor.battery.status.msg to true to show the battery status message
 PRODUCT_VENDOR_PROPERTIES += vendor.battery.status.msg=false
+
+# fw_printenv
+FW_ENV_SUPPORT := true
+ifneq ($(FW_ENV_SUPPORT),)
+PRODUCT_COPY_FILES += \
+    $(OUT_DIR)/target/product/$(firstword $(PRODUCT_DEVICE))/obj/UBOOT_OBJ/tools/env/fw_printenv:$(TARGET_COPY_OUT_VENDOR)/bin/fw_printenv \
+    $(OUT_DIR)/target/product/$(firstword $(PRODUCT_DEVICE))/obj/UBOOT_OBJ/tools/env/fw_printenv:$(TARGET_COPY_OUT_VENDOR)/bin/fw_setenv \
+    $(ARM_TOOLCAIN)/aarch64-none-linux-gnu/libc/lib64/ld-2.30.so:$(TARGET_COPY_OUT_VENDOR)/lib64/ld-linux-aarch64.so.1 \
+    $(ARM_TOOLCAIN)/aarch64-none-linux-gnu/libc/lib64/libc-2.30.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc.so.6 \
+    $(CONFIG_REPO_PATH)/common/init/tn-u-boot-fw-env.sh:$(TARGET_COPY_OUT_VENDOR)/bin/tn-u-boot-fw-env.sh
+endif
