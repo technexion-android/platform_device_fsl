b8c5175aeebb0f0b2b9efaef36f45f0004f16bf7
Fang Hui <hui.fang@nxp.com>
2024-07-31 22:35:55 +0800

    MA-22698-4 Bring up os08a20 on evk_95
    Add related config files and libs.
    
    Change-Id: I4c8946bb24c85c027e6ca0aaf7f678bfe046f121
    Signed-off-by: Fang Hui <hui.fang@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/25499
    Reviewed-by: Anle Pan <anle.pan@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/25657

--- imx9/evk_95/evk_95.mk
+++ imx9/evk_95/evk_95.mk
@@ -311,7 +311,13 @@ PRODUCT_PACKAGES += \
     capture \
     libcamera-base \
     libcamera \
-    libyaml
+    libyaml \
+    libnxp_ipa_cam_helper \
+    ipa_nxp_neo \
+    config.yaml \
+    os08a20.yaml \
+    nxpneo_ipa_proxy \
+    ipa_nxp_neo.so.sign
 
 PRODUCT_SOONG_NAMESPACES += hardware/google/camera
 PRODUCT_SOONG_NAMESPACES += vendor/nxp-opensource/imx/camera
