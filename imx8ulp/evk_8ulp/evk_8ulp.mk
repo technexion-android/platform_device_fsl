650c32dc2d956aadcfa2b40fdf4159466c738790
Dandan Sun <dandan.sun@nxp.com>
2024-09-24 11:03:42 +0800

    MA-23099 load sensor config file for the refined sensor HAL
    load sensor_hal_configuration.xml to support rotate.
    
    Change-Id: I50b626e6939a683a33ec0fccc4153881b860f679
    Signed-off-by: Dandan Sun <dandan.sun@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/26431
    Reviewed-by: Bo Zhang <bo.zhang@nxp.com>

--- imx8ulp/evk_8ulp/evk_8ulp.mk
+++ imx8ulp/evk_8ulp/evk_8ulp.mk
@@ -110,7 +110,8 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-nxp-IIO-Subhal
 
 PRODUCT_COPY_FILES += \
-    vendor/nxp-opensource/imx/iio_sensor/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+    $(IMX_PATH)/imx/sensor/config/sensor_hal_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_hal_configuration.xml \
+    $(IMX_PATH)/imx/sensor/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 #Enable this to use dynamic partitions for the readonly partitions not touched by bootloader
 TARGET_USE_DYNAMIC_PARTITIONS ?= true
