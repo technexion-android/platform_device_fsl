1164f0bb9925a6fcee97bedde2587fe4f815c940
Tifa Chen <tifa.chen@technexion.com>
2024-10-22 02:42:29 -0700

    edm_g_imx8mp: Update kernel defconfig on Android 14
    On Android 14, kernel config are merged from severel files.
    Add vendor defconfig file for TN changes.

--- imx8m/edm_g_imx8mp/UbootKernelBoardConfig.mk
+++ imx8m/edm_g_imx8mp/UbootKernelBoardConfig.mk
@@ -16,10 +16,11 @@ TARGET_BOOTLOADER_CONFIG += imx8mp-evk-uuu:edm-g-imx8mp_android_defconfig
 
 TARGET_KERNEL_DEFCONFIG := gki_defconfig
 ifeq ($(LOADABLE_KERNEL_MODULE),true)
-TARGET_KERNEL_GKI_DEFCONF:= tn_imx8mp_gki.fragment
+TARGET_KERNEL_GKI_DEFCONF:= imx8mp_gki.fragment
 else
-TARGET_KERNEL_GKI_DEFCONF := tn_imx8_android_defconfig
+TARGET_KERNEL_GKI_DEFCONF := imx_v8_android_defconfig
 endif
+TARGET_KERNEL_VENDOR_DEFCONF := tn_imx8_android_defconfig
 
 ifeq ($(SIM8202_MODEM_ACTIVE),true)
 TARGET_KERNEL_ADDITION_DEFCONF := android_sim8202_addition_defconfig
