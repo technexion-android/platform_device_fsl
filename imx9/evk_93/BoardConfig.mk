7da6fd65029e8e2b34da93f840832bbeca3e230a
Hui Wang <hui.wang_7@nxp.com>
2024-10-24 10:28:37 +0800

    MA-22988 Fix anr issue on imx93
    There is ANR log "E ActivityManager: ANR in com.android.systemui" when
    imx93 boot up. Root cause com.android.systemui meets ANR due to system low
    response.
    Here config ro.hw_timeout_multiplier=3 to avoid hitting timeouts for imx93.
    Set imx93 default display module 720p
    
    ANR logs:
    E ActivityManager: ANR in com.android.systemui
    E ActivityManager: PID: 3894
    E ActivityManager: Reason: Input dispatching timed out (dd533bb
    NotificationShade (server) is not responding. Waited 5003ms for
    FocusEvent(hasFocus=true))
    
    Change-Id: I353f7556dcc59b70691c2e39d12fa7dd4596161f
    Signed-off-by: Hui Wang <hui.wang_7@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/26199
    Reviewed-by: Jessie Hao <juan.hao@nxp.com>

--- imx9/evk_93/BoardConfig.mk
+++ imx9/evk_93/BoardConfig.mk
@@ -144,10 +144,7 @@ BOARD_BOOTCONFIG += androidboot.lcd_density=240
 BOARD_BOOTCONFIG += androidboot.wificountrycode=CN
 BOARD_KERNEL_CMDLINE += moal.mod_para=wifi_mod_para_sd612.conf
 
-# low memory device build config
-ifeq ($(LOW_MEMORY),true)
 BOARD_BOOTCONFIG += androidboot.displaymode=720p
-endif
 
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 BOARD_BOOTCONFIG += androidboot.vendor.sysrq=1
