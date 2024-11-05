5a71c4f6cf01a5369de08a14062904cb473a4ccf
Tifa Chen <tifa.chen@technexion.com>
2024-11-05 01:50:05 -0800

    edm_g_imx8mp: add rfkill feature for wifi control
    issue: wifi can't turn on
    Need rfkill features for wifi control
    
    error log:
      edm_g_imx8mp:/ # ifconfig -a wlan0 up
      ifconfig: ioctl 8914: Operation not possible due to RF-kill

--- imx8m/edm_g_imx8mp/BoardConfig.mk
+++ imx8m/edm_g_imx8mp/BoardConfig.mk
@@ -124,6 +124,8 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(IMX_DEVICE_PATH)/device_framewor
 # needed module driver
 BOARD_VENDOR_KERNEL_MODULES += \
     $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/net/rfkill/rfkill.ko \
+    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/net/rfkill/rfkill-gpio.ko \
+    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/drivers/bluetooth/mx8_bt_rfkill.ko \
     $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/net/wireless/cfg80211.ko
 
 #ifeq ($(WIFI_BT_DEV),QCA9377)
