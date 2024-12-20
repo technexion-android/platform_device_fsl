f4aa0f0648ad2f275b84053f52ad0cbbd02181c0
Anle Pan <anle.pan@nxp.com>
2024-12-20 15:20:26 +0900

    MA-23208 Enable the build for isp_vvcam
    . Add build_vvcam to compile
    . package the vvcam related modules
    
    Change-Id: I0d156cb31a6788c04e16d2d4f997e3fd5edcd538
    Signed-off-by: Anle Pan <anle.pan@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/26973
    Reviewed-by: Zhipeng Wang <zhipeng.wang_1@nxp.com>

--- common/tools/imx-make.sh
+++ common/tools/imx-make.sh
@@ -186,7 +186,7 @@ fi
 if [ ${build_kernel_oot_module_flag} -eq 1 ] || [ -n "${build_kernel_modules}" ]; then
     soc_path=${soc_path} product_path=${product_path} nxp_git_path=${nxp_git_path} clean_build=${clean_build} \
         skip_config_or_clean=${skip_config_or_clean} make -C ./ -f ${nxp_git_path}/common/build/Makefile ${parallel_option} \
-        ${build_galcore} </dev/null || exit
+        ${build_vvcam} ${build_galcore} </dev/null || exit
 fi
 
 if [ ${build_android_flag} -eq 1 ] || [ ${build_whole_android_flag} -eq 1 ]; then
