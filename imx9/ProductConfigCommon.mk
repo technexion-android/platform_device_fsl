da797337810a1daf9c6e74ac8f3dd824b854cc34
Jindong Yue <jindong.yue@nxp.com>
2024-12-30 14:53:24 +0800

    MA-23292 audio: Disable pre-processing audio effects
    Some CTS cases fail when the FastCapture is enabled.
      android.media.audio.cts.AudioPreProcessingTest#test1_1NsCreateAndRelease
        W AudioFlinger: checkEffectCompatibility_l(): non HW effect Acoustic Echo Canceler on record thread AudioIn_E56 in fast mode
        E AudioEffect: set(): AudioFlinger could not create effect 7b491460-8d4d-11e0-bd61-0002a5d5c51b / ec7178ec-e5e1-4432-a3f4-4657e6795210, status: -22
        E AudioEffects-JNI: AudioEffect initCheck failed -3
        E AudioEffect-JAVA: Error code -3 when initializing AudioEffect.
    
    Use imx specific audio effects config file, which disables
    the pre-processing effects.
    
    Change-Id: I153f33f3bbf17ddbcd401f304416b291a7fc2fa1
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/27133
    Reviewed-by: Hui Fang <hui.fang@nxp.com>

--- imx9/ProductConfigCommon.mk
+++ imx9/ProductConfigCommon.mk
@@ -289,7 +289,7 @@ PRODUCT_PACKAGES += \
 
 
 PRODUCT_COPY_FILES += \
-    hardware/interfaces/audio/aidl/default/audio_effects_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects_config.xml \
+    vendor/nxp-opensource/imx/audio/default/audio_effects_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects_config.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
