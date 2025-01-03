4237294434ced6b722168135f240940177a0db63
Dandan Sun <dandan.sun@nxp.com>
2025-01-03 15:45:48 +0800

    MA-23302-2 remove vendor/nxp/fsl-proprietary/sensor/fsl-sensor.mk
    fsl_sensor_fusion is not needed, so remove fsl-sensor.mk
    
    Change-Id: Ibbc9533119e96a007f536ff0c924360cd94c41c9
    Signed-off-by: Dandan Sun <dandan.sun@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/27192
    Reviewed-by: Bo Zhang <bo.zhang@nxp.com>

--- imx8m/evk_8mn/AndroidBoard.mk
+++ imx8m/evk_8mn/AndroidBoard.mk
@@ -4,7 +4,6 @@ include $(CONFIG_REPO_PATH)/common/build/dtbo.mk
 include $(CONFIG_REPO_PATH)/common/build/imx-recovery.mk
 include $(CONFIG_REPO_PATH)/common/build/gpt.mk
 include $(FSL_PROPRIETARY_PATH)/fsl-proprietary/media-profile/media-profile.mk
-include $(FSL_PROPRIETARY_PATH)/fsl-proprietary/sensor/fsl-sensor.mk
 -include $(IMX_MEDIA_CODEC_XML_PATH)/mediacodec-profile/mediacodec-profile.mk
 
 BOARD_PACK_RADIOIMAGES += bootloader.img
