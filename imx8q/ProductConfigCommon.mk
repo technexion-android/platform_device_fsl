f0250e95b3a7446b967875e8cb34fa84571cfcb9
Xuegang Liu <xuegang.liu@nxp.com>
2025-02-19 04:01:51 +0800

    MA-23441 Set PRODUCT_SHIPPING_API_LEVEL to 36 on Android16
    Set product shipping api level to 36 on Android16
    
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>
    Change-Id: Iaa4599efb40f956e4b44cb3754d587eed6a53de6
    Reviewed-on: http://androidsource.nxp.com/project/27552
    Reviewed-by: Elven Wang <elven.wang@nxp.com>
    Reviewed-by: Jessie Hao <juan.hao@nxp.com>

--- imx8q/ProductConfigCommon.mk
+++ imx8q/ProductConfigCommon.mk
@@ -42,7 +42,7 @@ PRODUCT_MANUFACTURER := nxp
 # related to the definition and load of library modules
 TARGET_BOARD_PLATFORM := imx
 
-PRODUCT_SHIPPING_API_LEVEL := 35
+PRODUCT_SHIPPING_API_LEVEL := 36
 
 # -------@block_app-------
 PRODUCT_PROPERTY_OVERRIDES += \
