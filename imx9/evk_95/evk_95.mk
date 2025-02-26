006674bb47d07e2f893d7f54c872415966a14170
Jessie Hao <juan.hao@nxp.com>
2025-02-26 16:17:17 -0600

    MA-23482 Enable GPU profiler support for evk_95.
    Add libgpudataproducer and config graphics.gpu.profiler.support=true to
    enable GPU profiler support for evk_95.
    
    Fix the below SELinux issues:
    avc:  denied  { map } for  comm="ctsgraphicsgpuc"
    path="/vendor/lib64/libgpudataproducer.so" dev="overlay" ino=82
    scontext=u:r:shell:s0 tcontext=u:object_r:vendor_file:s0 tclass=file
    permissive=1
    
    avc:  denied  { read } for  name="libgpudataproducer.so" dev="dm-3"
    ino=4003793 scontext=u:r:crash_dump:s0
    tcontext=u:object_r:profiling_mali_gpu:s0 tclass=file permissive=0
    
    Change-Id: If37845f35d8bae09b907850f353e9a7766f313ca
    Signed-off-by: Jessie Hao <juan.hao@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/27621
    Reviewed-by: Bo Zhang <bo.zhang@nxp.com>
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/27632

--- imx9/evk_95/evk_95.mk
+++ imx9/evk_95/evk_95.mk
@@ -354,6 +354,7 @@ PRODUCT_PACKAGES += \
     libarm_egl_properties_sysprop \
     libarm_gralloc_properties_sysprop \
     libarm_mali_config_sysprops \
+    libgpudataproducer \
     libGLES_mali \
     libOpenCL \
     vulkan.mali
@@ -367,6 +368,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_VENDOR_PROPERTIES += \
     ro.hardware.egl = mali \
     ro.hardware.vulkan = mali
+
+PRODUCT_VENDOR_PROPERTIES += \
+    graphics.gpu.profiler.support=true
 
 # -------@block_wifi-------
 
