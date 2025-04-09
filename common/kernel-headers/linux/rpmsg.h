7430042b4ffcdd2e322d629f7c4e38b770a31cae
He Zhai <zhai.he@nxp.com>
2025-04-09 11:05:18 +0900

    MA-23602-2 add rpmsg.h to common/kernel-headers
    Command:
           ./device/nxp/common/tools/update_kerenl_header.sh
    
    Change-Id: I3603cc5c81e5ac18effd37ddae9d2bf0e050e582
    Signed-off-by: He Zhai <zhai.he@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/27906
    Reviewed-by: Zhipeng Wang <zhipeng.wang_1@nxp.com>

---
+++ common/kernel-headers/linux/rpmsg.h
@@ -0,0 +1,24 @@
+/*
+ * This file is auto-generated. Modifications will be lost.
+ *
+ * See https://android.googlesource.com/platform/bionic/+/master/libc/kernel/
+ * for more information.
+ */
+#ifndef _UAPI_RPMSG_H_
+#define _UAPI_RPMSG_H_
+#include <linux/ioctl.h>
+#include <linux/types.h>
+#define RPMSG_ADDR_ANY 0xFFFFFFFF
+struct rpmsg_endpoint_info {
+  char name[32];
+  __u32 src;
+  __u32 dst;
+};
+#define RPMSG_CREATE_EPT_IOCTL _IOW(0xb5, 0x1, struct rpmsg_endpoint_info)
+#define RPMSG_DESTROY_EPT_IOCTL _IO(0xb5, 0x2)
+#define RPMSG_CREATE_DEV_IOCTL _IOW(0xb5, 0x3, struct rpmsg_endpoint_info)
+#define RPMSG_RELEASE_DEV_IOCTL _IOW(0xb5, 0x4, struct rpmsg_endpoint_info)
+#define RPMSG_GET_OUTGOING_FLOWCONTROL _IOR(0xb5, 0x5, int)
+#define RPMSG_SET_INCOMING_FLOWCONTROL _IOR(0xb5, 0x6, int)
+#define RPMSG_CHRE_GET_STATE _IOR(0xb5, 0x7, int)
+#endif
