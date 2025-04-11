5372e47ce9389d1a63a9e9f31dab8d879cd0ba2d
Xuegang Liu <xuegang.liu@nxp.com>
2025-04-11 21:33:12 +0800

    MA-23625 Use 28GB bpt by default on the boards with 32GB eMMC
    Below are the reasons to use 28GB GPT by default:
    1. Imx95 eMMC 64GB, imx93 and 8mq eMMC 16GB, other boards eMMC 32GB.
    2. Use 13GB bpt the data partition is only 9GB when run CTS test may
       meet data partition full issue.
    3. u-boot has include a patch to speed eMMC erase operation now
       (bd2a6d30219 LFU-850 mmc: Optimize eMMC erase speed).
    4. Use 28GB bpt by default on the boards with 32GB eMMC are reasonable.
    
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>
    Change-Id: Ic732d6c4a6514bbb1408cf4146aab932541f60d0
    Reviewed-on: http://androidsource.nxp.com/project/28016
    Reviewed-by: Faqiang Zhu <faqiang.zhu@nxp.com>

--- imx8m/evk_8mp/BoardConfig.mk
+++ imx8m/evk_8mp/BoardConfig.mk
@@ -44,10 +44,10 @@ TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 
 # Support gpt
 ifeq ($(TARGET_USE_DYNAMIC_PARTITIONS),true)
-  BOARD_BPT_INPUT_FILES += $(CONFIG_REPO_PATH)/common/partition/device-partitions-13GB-ab_super.bpt
-  ADDITION_BPT_PARTITION = partition-table-28GB:$(CONFIG_REPO_PATH)/common/partition/device-partitions-28GB-ab_super.bpt \
-                           partition-table-dual:$(CONFIG_REPO_PATH)/common/partition/device-partitions-13GB-ab-dual-bootloader_super.bpt \
-                           partition-table-28GB-dual:$(CONFIG_REPO_PATH)/common/partition/device-partitions-28GB-ab-dual-bootloader_super.bpt
+  BOARD_BPT_INPUT_FILES += $(CONFIG_REPO_PATH)/common/partition/device-partitions-28GB-ab_super.bpt
+  ADDITION_BPT_PARTITION = partition-table-13GB:$(CONFIG_REPO_PATH)/common/partition/device-partitions-13GB-ab_super.bpt \
+                           partition-table-dual:$(CONFIG_REPO_PATH)/common/partition/device-partitions-28GB-ab-dual-bootloader_super.bpt \
+                           partition-table-13GB-dual:$(CONFIG_REPO_PATH)/common/partition/device-partitions-13GB-ab-dual-bootloader_super.bpt
 else
   ifeq ($(IMX_NO_PRODUCT_PARTITION),true)
     BOARD_BPT_INPUT_FILES += $(CONFIG_REPO_PATH)/common/partition/device-partitions-13GB-ab-no-product.bpt
