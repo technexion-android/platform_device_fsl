d58422c097da77b234ed7fc9c9dc701ae5fba7fe
Maximus <maximus.sun@nxp.com>
2025-04-25 10:24:22 +0900

    MA-23673 Fix 8mm-evk-ddr4 build failed bug after u-boot 2025 update
    i.MX 8MM evk ddr4 failed to build because the u-boot device tree copy from
    a wrong path after u-boot 2025 update.
    This path fix this bug by modify the device tree path to 'upstream' folder.
    
    Change-Id: I57312e1b0992608f52aa5b35668bd2256f63b450
    Signed-off-by: Maximus <maximus.sun@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/28419
    Reviewed-by: Ji Luo <ji.luo@nxp.com>
    (cherry picked from commit df9d9a38e7989012b282b6c0c9a830d592cff427)
    Reviewed-on: http://androidsource.nxp.com/project/28420

--- imx8m/evk_8mm/AndroidUboot.sh
+++ imx8m/evk_8mm/AndroidUboot.sh
@@ -41,7 +41,7 @@ build_imx_uboot()
 	cp ${UBOOT_OUT}/tools/mkimage  ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/mkimage_uboot
 
 	if [ "`echo $2 | cut -d '-' -f2`" = "ddr4" ]; then
-		cp ${UBOOT_OUT}/arch/arm/dts/imx8mm-ddr4-evk.dtb ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/
+		cp ${UBOOT_OUT}/dts/upstream/src/arm64/freescale/imx8mm-ddr4-evk.dtb ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/
 		cp ${FSL_PROPRIETARY_PATH}/linux-firmware-imx/firmware/ddr/synopsys/ddr4* ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/
 	else
 		cp ${UBOOT_OUT}/arch/arm/dts/imx8mm-evk.dtb  ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/
