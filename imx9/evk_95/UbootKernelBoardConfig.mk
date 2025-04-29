5db0032600f4e7f5a9558f7b893c005c8c3eebc9
Maximus <maximus.sun@nxp.com>
2025-04-29 18:23:29 +0900

    MA-23691 Add "rbidx_blob" defconfig for i.MX 95 15x15 platform
    Add rbidx_blob defconfig to the default compilation list for i.MX 95
    15x15 platform.
    
    Change-Id: I2033cf1e83d9455f71c067964c0b64ac1f1068bf
    Signed-off-by: Maximus <maximus.sun@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/28498
    Reviewed-by: Ji Luo <ji.luo@nxp.com>

--- imx9/evk_95/UbootKernelBoardConfig.mk
+++ imx9/evk_95/UbootKernelBoardConfig.mk
@@ -12,6 +12,7 @@ TARGET_BOOTLOADER_CONFIG += imx95-trusty-dual:imx95_19x19_evk_android_trusty_dua
 TARGET_BOOTLOADER_CONFIG += imx95-trusty-secure-unlock-dual:imx95_19x19_evk_android_trusty_secure_unlock_dual_defconfig
 TARGET_BOOTLOADER_CONFIG += imx95-trusty-verdin-dual:imx95_19x19_verdin_android_trusty_dual_defconfig
 TARGET_BOOTLOADER_CONFIG += imx95-trusty-15x15-dual:imx95_15x15_evk_android_trusty_dual_defconfig
+TARGET_BOOTLOADER_CONFIG += imx95-trusty-15x15-rbidx-blob-dual:imx95_15x15_evk_android_trusty_rbidx_blob_dual_defconfig
 TARGET_BOOTLOADER_CONFIG += imx95-rpmsg:imx95_19x19_evk_android_rpmsg_defconfig
 TARGET_BOOTLOADER_CONFIG += imx95-evk-uuu:imx95_19x19_evk_android_uuu_defconfig
 TARGET_BOOTLOADER_CONFIG += imx95-verdin-uuu:imx95_19x19_verdin_android_uuu_defconfig
