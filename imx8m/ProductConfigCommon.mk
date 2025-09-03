e648f1eaf57b31ebebf02dda1978529e50dc074e
Ji Luo <ji.luo@nxp.com>
2025-09-03 18:02:30 +0800

    MA-24066 Declare RKP only for tee and strongbox
    For android 16 and newer versions must be RKP only, set
    corresponding properties to make the GTS tests pass.
    
    Test:
      com.google.android.gts.security.AttestationRootHostTest#testRkpOnlyPropertyStrongBox
      com.google.android.gts.security.AttestationRootHostTest#testRkpOnlyPropertyTee
    
    Change-Id: Ic4325ffc32029469e909ef92b55a3de7c7ad1db0
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/29689
    Reviewed-by: Maximus Sun <maximus.sun@nxp.com>

--- imx8m/ProductConfigCommon.mk
+++ imx8m/ProductConfigCommon.mk
@@ -184,6 +184,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # RKPD
 PRODUCT_PRODUCT_PROPERTIES += \
     remote_provisioning.enable_rkpd=true \
+    remote_provisioning.tee.rkp_only=1 \
+    remote_provisioning.strongbox.rkp_only \
     remote_provisioning.hostname=remoteprovisioning.googleapis.com
 
 # health
