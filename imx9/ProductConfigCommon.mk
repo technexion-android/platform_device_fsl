e75d9dca9b132e192262de055f829d8f62db04de
Ji Luo <ji.luo@nxp.com>
2025-09-04 11:50:49 +0800

    MA-24066-2 Assign the rkp property with correct value
    Correct the strongbox rkp property value.
    
    Change-Id: Ia1a0c62ee7e233270244b5949fef9441939b0d96
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/29696
    Reviewed-by: Maximus Sun <maximus.sun@nxp.com>

--- imx9/ProductConfigCommon.mk
+++ imx9/ProductConfigCommon.mk
@@ -183,7 +183,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     remote_provisioning.enable_rkpd=true \
     remote_provisioning.tee.rkp_only=1 \
-    remote_provisioning.strongbox.rkp_only \
+    remote_provisioning.strongbox.rkp_only=1 \
     remote_provisioning.hostname=remoteprovisioning.googleapis.com
 
 # health
