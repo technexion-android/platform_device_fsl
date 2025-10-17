c426602a71f79a1ece741415ebddb747968e0feb
Tifa Chen <tifa.chen@technexion.com>
2025-10-17 01:28:30 -0700

    edm_imx95: Add edm_imx95 support on Android (3) - bootlaoder
    1. OEI firmware will be generated by specific DDR_CONFIG.
       Then compiler bootloader image with corresponding pre images.
    
    2. Apply single bootloader patch from user guide 7.1.4

--- imx9/edm_imx95/UbootKernelBoardConfig.mk
+++ imx9/edm_imx95/UbootKernelBoardConfig.mk
@@ -3,20 +3,11 @@ TARGET_BOOTLOADER_POSTFIX := bin
 UBOOT_POST_PROCESS := true
 
 # u-boot target
-TARGET_BOOTLOADER_CONFIG := imx95:imx95_19x19_evk_android_defconfig
-TARGET_BOOTLOADER_CONFIG += imx95-verdin:imx95_19x19_verdin_android_defconfig
-TARGET_BOOTLOADER_CONFIG += imx95-15x15:imx95_15x15_evk_android_defconfig
-TARGET_BOOTLOADER_CONFIG += imx95-dual:imx95_19x19_evk_android_dual_defconfig
-TARGET_BOOTLOADER_CONFIG += imx95-15x15-dual:imx95_15x15_evk_android_dual_defconfig
-TARGET_BOOTLOADER_CONFIG += imx95-trusty-dual:imx95_19x19_evk_android_trusty_dual_defconfig
-TARGET_BOOTLOADER_CONFIG += imx95-trusty-secure-unlock-dual:imx95_19x19_evk_android_trusty_secure_unlock_dual_defconfig
-TARGET_BOOTLOADER_CONFIG += imx95-trusty-verdin-dual:imx95_19x19_verdin_android_trusty_dual_defconfig
-TARGET_BOOTLOADER_CONFIG += imx95-trusty-15x15-dual:imx95_15x15_evk_android_trusty_dual_defconfig
-TARGET_BOOTLOADER_CONFIG += imx95-trusty-15x15-rbidx-blob-dual:imx95_15x15_evk_android_trusty_rbidx_blob_dual_defconfig
-TARGET_BOOTLOADER_CONFIG += imx95-rpmsg:imx95_19x19_evk_android_rpmsg_defconfig
-TARGET_BOOTLOADER_CONFIG += imx95-evk-uuu:imx95_19x19_evk_android_uuu_defconfig
-TARGET_BOOTLOADER_CONFIG += imx95-verdin-uuu:imx95_19x19_verdin_android_uuu_defconfig
-TARGET_BOOTLOADER_CONFIG += imx95-15x15-evk-uuu:imx95_15x15_evk_android_uuu_defconfig
+TARGET_BOOTLOADER_BASE_CONFIG := edm-imx95_defconfig
+TARGET_BOOTLOADER_CONFIG := imx95:edm-imx95_android_defconfig
+TARGET_BOOTLOADER_CONFIG += imx95-evk-uuu:edm-imx95_android_defconfig
+TARGET_BOOTLOADER_CONFIG += imx95-16GB:edm-imx95_android_defconfig
+TARGET_BOOTLOADER_CONFIG += imx95-16GB-evk-uuu:edm-imx95_android_defconfig
 
 TARGET_KERNEL_DEFCONFIG := gki_defconfig
 ifeq ($(LOADABLE_KERNEL_MODULE),true)
