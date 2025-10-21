22a021010e5c6082966c8009576e8c335261633e
Tifa Chen <tifa.chen@technexion.com>
2025-10-20 22:40:15 -0700

    axon_imx8mp: Upgrade to Android 15
    Generate evk_8mp patch from android android-14.0.0_2.2.0 to android-15.0.0_2.0.0.
    And then apply into axon_imx8mp.

--- imx8m/axon_imx8mp/AndroidBoard.mk
+++ imx8m/axon_imx8mp/AndroidBoard.mk
@@ -5,5 +5,4 @@ include $(CONFIG_REPO_PATH)/common/build/imx-recovery.mk
 include $(CONFIG_REPO_PATH)/common/build/gpt.mk
 #TODO add imx8mp target
 include $(IMX_DEVICE_PATH)/media-profile.mk
-include $(FSL_PROPRIETARY_PATH)/fsl-proprietary/sensor/fsl-sensor.mk
 -include $(IMX_MEDIA_CODEC_XML_PATH)/mediacodec-profile/mediacodec-profile.mk
