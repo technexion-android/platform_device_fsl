2dc8cc71675810c1b4de006367c5ae43e412b8c4
Tifa Chen <tifa.chen@technexion.com>
2025-10-29 18:17:50 -0700

    pico_imx8mm: enable oemlock service on Android 15

--- imx8m/pico_imx8mm/pico_imx8mm.mk
+++ imx8m/pico_imx8mm/pico_imx8mm.mk
@@ -217,12 +217,12 @@ endif
 # Add oem unlocking option in settings.
 PRODUCT_PROPERTY_OVERRIDES += ro.frp.pst=/dev/block/by-name/presistdata
 
-ifeq ($(PRODUCT_IMX_TRUSTY),true)
+#ifeq ($(PRODUCT_IMX_TRUSTY),true)
 #Oemlock HAL support
 PRODUCT_PACKAGES += \
     android.hardware.oemlock-service.imx \
     android.hardware.oemlock-service-software.imx
-endif
+#endif
 
 # Secretkeeper HAL
 PRODUCT_PACKAGES += \
