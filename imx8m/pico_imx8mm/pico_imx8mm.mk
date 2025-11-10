e575cd9e739e981cc028ba3d41f97900dafb2fa2
Tifa Chen <tifa.chen@technexion.com>
2025-11-09 22:42:36 -0800

    pico_imx8mm: Correct audio codec config file
    pico_imx8mm is used TI tlv320aic31xx codec.

--- imx8m/pico_imx8mm/pico_imx8mm.mk
+++ imx8m/pico_imx8mm/pico_imx8mm.mk
@@ -267,6 +267,7 @@ $(call inherit-product-if-exists, vendor/nxp-private/widevine/apex/device.mk)
 # Audio card json
 PRODUCT_COPY_FILES += \
     $(CONFIG_REPO_PATH)/common/audio-json/wm8960_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/configs/audio/wm8960_config.json \
+    $(CONFIG_REPO_PATH)/common/audio-json/tlv320aic31xx_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/configs/audio/tlv320aic31xx_config.json \
     $(CONFIG_REPO_PATH)/common/audio-json/readme.txt:$(TARGET_COPY_OUT_VENDOR)/etc/configs/audio/readme.txt
 
 PRODUCT_COPY_FILES += \
