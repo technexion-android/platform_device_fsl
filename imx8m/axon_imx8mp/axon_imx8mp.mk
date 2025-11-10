cd5225f3871f1a35fa3fe43cd98547a0ea03c6ab
Tifa Chen <tifa.chen@technexion.com>
2025-11-09 22:37:09 -0800

    axon_imx8mp: Correct audio codec config file
    axon_imx8mp is used TI tlv320aic31xx codec.

--- imx8m/axon_imx8mp/axon_imx8mp.mk
+++ imx8m/axon_imx8mp/axon_imx8mp.mk
@@ -296,7 +296,7 @@ $(call inherit-product-if-exists, vendor/nxp-private/widevine/apex/device.mk)
 
 # Audio card json
 PRODUCT_COPY_FILES += \
-    $(CONFIG_REPO_PATH)/common/audio-json/tlv320aic3x_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/configs/audio/tlv320aic3x_config.json \
+    $(CONFIG_REPO_PATH)/common/audio-json/tlv320aic31xx_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/configs/audio/tlv320aic31xx_config.json \
     $(CONFIG_REPO_PATH)/common/audio-json/micfil_s32_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/configs/audio/micfil_s32_config.json \
     $(CONFIG_REPO_PATH)/common/audio-json/hdmi_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/configs/audio/hdmi_config.json \
     $(CONFIG_REPO_PATH)/common/audio-json/btsco_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/configs/audio/btsco_config.json \
