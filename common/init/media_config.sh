784f1e9a68ac0db4f5c783a0e30c0a474514fd0a
Tifa Chen <tifa.chen@technexion.com>
2025-12-16 10:10:16 +0800

    common: set media profile based on camera product name
    With low resolution camera product, media codec should use lower
    recording capability profile. For example, 720p. Set camera product
    name on uboot in bootargs env with androidboot.camera.product.

--- common/init/media_config.sh
+++ common/init/media_config.sh
@@ -5,13 +5,16 @@ FB_MODE_PATH="/sys/class/graphics/fb0/virtual_size"
 # SOC
 SOC=$(getprop ro.boot.soc_type)
 
+# camera module
+CAM=$(getprop ro.boot.camera.product)
+
 if [ -f "$FB_MODE_PATH" ]; then
     FB_SIZE=$(cat $FB_MODE_PATH)
     WIDTH=${FB_SIZE%%,*}
     HEIGHT=${FB_SIZE##*,}
 
-    # low resolution display
-    if [ "$WIDTH" -lt 1920 ] || [ "$HEIGHT" -lt 1080 ] ; then
+    # low resolution on display or camera module
+    if [ "$WIDTH" -lt 1920 ] || [ "$HEIGHT" -lt 1080 ] || [ "$CAM" = "ar0144" ] ; then
         case "$SOC" in
             "imx95")
                 TARGET_PROFILE="_95-ap1302"
